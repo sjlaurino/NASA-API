031e1ccbdd58a9b5b5fb009c69485f04c47513f6
Steven Laurino <sjlaurino@gmail.com>
2019-02-20 13:14:20 -0700

    Added Note

--- app/components/nasaController.js
+++ app/components/nasaController.js
@@ -6,7 +6,7 @@ let _nasaService = new nasaService()
 function drawObject() {
   let nasaAPOD = _nasaService.Object
   let template = ''
-  // object.forEach(o => {
+  // object.forEach(o => { DONT NEED FOR EACH. ONLY ONE OBJECT
   template += nasaAPOD.grabTemplate()
   // })
   document.querySelector('.object').innerHTML = template
